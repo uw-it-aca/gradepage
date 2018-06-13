c7a24d0b092ba45effa0259d1ef300e04ed4573c
Jim Laney <jlaney@uw.edu>
2018-06-13 16:08:35 -0700

    adding test coverage for notifications

--- course_grader/test/dao/test_notification.py
+++ course_grader/test/dao/test_notification.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.test.utils import override_settings
 from uw_pws.util import fdao_pws_override
 from uw_sws.util import fdao_sws_override
 from course_grader.dao.person import PWS
@@ -10,23 +11,59 @@ from course_grader.dao.notification import *
 @fdao_sws_override
 @fdao_pws_override
 class NotificationDAOFunctionsTest(TestCase):
-    def test_graderoster_people(self):
-        section = get_section_by_label('2013,summer,CSS,161/A')
-        user = PWS().get_person_by_regid('FBB38FE46A7C11D5A4AE0004AC494FFE')
-        graderoster = get_graderoster(section, user, requestor=user)
+    def setUp(self):
+        pws = PWS()
+        self.section = get_section_by_label('2013,summer,CSS,161/A')
+        self.user = pws.get_person_by_regid('FBB38FE46A7C11D5A4AE0004AC494FFE')
+        self.graderoster = get_graderoster(
+            self.section, self.user, requestor=self.user)
 
-        p = graderoster_people(graderoster)
+    def test_graderoster_people(self):
+        p = graderoster_people(self.graderoster)
         self.assertEquals(len(p), 3)
 
     def test_create_recipient_list(self):
-        section = get_section_by_label('2013,summer,CSS,161/A')
-        user = PWS().get_person_by_regid('FBB38FE46A7C11D5A4AE0004AC494FFE')
-        graderoster = get_graderoster(section, user, requestor=user)
-
-        people = graderoster_people(graderoster)
+        people = graderoster_people(self.graderoster)
         r = create_recipient_list(people)
         self.assertEquals(len(r), 3)
         r.sort()
         self.assertEquals(r[0], 'bill@uw.edu')
         self.assertEquals(r[1], 'fred@uw.edu')
         self.assertEquals(r[2], 'james@uw.edu')
+
+    @override_settings(CURRENT_DATETIME_OVERRIDE="2013-08-04 10:00:00")
+    def test_create_message_success(self):
+        for item in self.graderoster.items:
+            item.status_code = '200'
+
+        (subject, text, html) = create_message(self.graderoster, self.user)
+
+        self.assertEquals(
+            subject, 'Bill Teacher submitted four grades for CSS 161 A')
+        self.assertEquals(text, '\nBill Teacher submitted grades for four students to the Registrar on August 04 at 10:00 AM UTC.  These grades have been successfully processed and will be available to the students via MyUW.\n\nTo view or print a copy of this grade submission for your records, go to: http://localhost/section/2013-summer-CSS-161-A-FBB38FE46A7C11D5A4AE0004AC494FFE\n\nNo changes can be made through GradePage.  To change submitted grades use the Change of Grade form: https://depts.washington.edu/registra/staffFaculty/gradeChange/\n\n\n')
+        self.assertEquals(html, '\n<p>Bill Teacher submitted grades for four students to the Registrar on August 04 at 10:00 AM UTC.  These grades have been successfully processed and will be available to the students via MyUW.\n</p>\n\n<a href="http://localhost/section/2013-summer-CSS-161-A-FBB38FE46A7C11D5A4AE0004AC494FFE">View or print a copy of this grade submission for your records.</a>\n\n<p>No changes can be made through GradePage.  To change submitted grades, use the <a href="https://depts.washington.edu/registra/staffFaculty/gradeChange/">Change of Grade form</a>.</p>\n\n\n')
+
+    @override_settings(CURRENT_DATETIME_OVERRIDE="2013-08-04 10:00:00")
+    def test_create_message_failure(self):
+        for item in self.graderoster.items:
+            item.status_code = '400'
+
+        (subject, text, html) = create_message(self.graderoster, self.user)
+
+        self.assertEquals(
+            subject, 'Failed grade submission attempt for CSS 161 A')
+        self.assertEquals(text, '\nBill Teacher unsuccessfully submitted grades for four students to the Registrar on August 04 at 10:00 AM UTC.  There was a problem processing these grades and they have not been submitted.\n\nNo changes can be made through GradePage.  To change submitted grades, use the Change of Grade form: https://depts.washington.edu/registra/staffFaculty/gradeChange/\n\n\n')
+        self.assertEquals(html, '\n<p>Bill Teacher unsuccessfully submitted grades for four students to the Registrar on August 04 at 10:00 AM UTC.  There was a problem processing these grades and they have not been submitted.  For more information, see the <a href="http://localhost/section/2013-summer-CSS-161-A-FBB38FE46A7C11D5A4AE0004AC494FFE">grade submission receipt</a>.</p>\n\n<p>No changes can be made through GradePage.  To change submitted grades, use the <a href="https://depts.washington.edu/registra/staffFaculty/gradeChange/">Change of Grade form</a>.</p>\n\n\n')
+
+    @override_settings(CURRENT_DATETIME_OVERRIDE="2013-08-04 10:00:00")
+    def test_create_message_partial(self):
+        for item in self.graderoster.items:
+            item.status_code = '200'
+        self.graderoster.items[0].status_code = '400'
+
+        (subject, text, html) = create_message(self.graderoster, self.user)
+
+        self.assertEquals(
+            subject, 'Failed grade submission attempt for CSS 161 A')
+        self.assertEquals(text, '\nBill Teacher submitted grades for four students to the Registrar on August 04 at 10:00 AM UTC.  These grades have been successfully processed and will be available to the students via MyUW.\n\nHowever, one grade failed to be submitted successfully.  For more information, see the grade submission receipt: http://localhost/section/2013-summer-CSS-161-A-FBB38FE46A7C11D5A4AE0004AC494FFE\n\nNo changes can be made through GradePage.  To change submitted grades, use the Change of Grade form: https://depts.washington.edu/registra/staffFaculty/gradeChange/\n\n\n')
+        self.assertEquals(html, '\n<p>Bill Teacher submitted grades for four students to the Registrar on August 04 at 10:00 AM UTC.  These grades have been successfully processed and will be available to the students via MyUW.\n</p>\n\n<p>However, one grade failed to be submitted successfully.  For more information, see the <a href="http://localhost/section/2013-summer-CSS-161-A-FBB38FE46A7C11D5A4AE0004AC494FFE">grade submission receipt</a>.</p>\n\n<p>No changes can be made through GradePage.  To change submitted grades, use the <a href="https://depts.washington.edu/registra/staffFaculty/gradeChange/">Change of Grade form</a>.</p>\n\n\n')
---
+++ course_grader/test/test_exceptions.py
@@ -0,0 +1,66 @@
+from django.test import TestCase
+from course_grader.exceptions import *
+
+
+class ExceptionsTest(TestCase):
+    def test_InvalidUser(self):
+        try:
+            raise InvalidUser('javerage')
+        except InvalidUser as ex:
+            self.assertEquals(str(ex), "Invalid identifier 'javerage'")
+
+    def test_InvalidTerm(self):
+        try:
+            raise InvalidTerm()
+        except InvalidTerm as ex:
+            self.assertEquals(str(ex), "No submission information available")
+
+    def test_InvalidSection(self):
+        try:
+            raise InvalidSection()
+        except InvalidSection as ex:
+            self.assertEquals(str(ex), "Section not found.")
+
+    def test_MissingInstructorParam(self):
+        try:
+            raise MissingInstructorParam()
+        except MissingInstructorParam as ex:
+            self.assertEquals(str(ex), "Missing instructor UWRegID")
+
+    def test_ReceiptNotFound(self):
+        try:
+            raise ReceiptNotFound()
+        except ReceiptNotFound as ex:
+            self.assertEquals(str(ex), "No submission information available")
+
+    def test_GradingPeriodNotOpen(self):
+        try:
+            raise GradingPeriodNotOpen()
+        except GradingPeriodNotOpen as ex:
+            self.assertEquals(str(ex), ("Grade submission is not open for "
+                                        "this section."))
+
+    def test_SecondaryGradingEnabled(self):
+        try:
+            raise SecondaryGradingEnabled()
+        except SecondaryGradingEnabled as ex:
+            self.assertEquals(str(ex), (
+               "Secondary grading is enabled for this course. Final grades "
+               "must be submitted separately for each secondary section."))
+
+    def test_GradingNotPermitted(self):
+        section = None
+        person = None
+        try:
+            raise GradingNotPermitted(section, person)
+        except GradingNotPermitted as ex:
+            self.assertEquals(
+                str(ex),
+                "You are not authorized to submit grades for this section.")
+
+    def test_OverrideNotPermitted(self):
+        try:
+            raise OverrideNotPermitted()
+        except OverrideNotPermitted as ex:
+            self.assertEquals(
+                str(ex), "Cannot submit grades while using admin override.")
