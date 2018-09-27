3b703002f3232941b9724fc36dbc5bbe2e5bd299
Jim Laney <jlaney@uw.edu>
2018-09-27 12:09:20 -0700

    drops SessionAuthenticationMiddleware

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -28,7 +28,6 @@ MIDDLEWARE = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.RemoteUserMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
