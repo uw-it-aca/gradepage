e3fd1cdc2e227ba14dae93f125e62ef259e6f52a
Jim Laney <jlaney@uw.edu>
2019-02-27 10:25:39 -0800

    adds graderoster dao tests

---
+++ course_grader/test/dao/test_graderoster.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from uw_pws.util import fdao_pws_override
+from uw_sws.util import fdao_sws_override
+from course_grader.dao.person import PWS
+from course_grader.dao.section import get_section_by_label
+from course_grader.dao.graderoster import graderoster_for_section
+from course_grader.exceptions import (
+    GradingNotPermitted, ReceiptNotFound, GradingPeriodNotOpen)
+
+
+@fdao_sws_override
+@fdao_pws_override
+class GraderosterDAOFunctionsTest(TestCase):
+    def test_graderoster_for_section(self):
+        section = get_section_by_label('2013,spring,TRAIN,101/A')
+        user = PWS().get_person_by_regid('FBB38FE46A7C11D5A4AE0004AC494FFE')
+        gr = graderoster_for_section(section, user, user)
+
+        self.assertEqual(len(gr.items), 1000)
+
+    def test_submitted_graderoster_for_section(self):
+        section = get_section_by_label('2013,spring,TRAIN,101/B')
+        user = PWS().get_person_by_regid('FBB38FE46A7C11D5A4AE0004AC494FFE')
+
+        self.assertRaises(
+            ReceiptNotFound, graderoster_for_section, section, user, user,
+            submitted_graderosters_only=True)
+
+    def test_graderoster_not_permitted(self):
+        section = get_section_by_label('2013,spring,TRAIN,100/AA')
+        user = PWS().get_person_by_regid('9136CCB8F66711D5BE060004AC494FFE')
+
+        self.assertRaises(
+            GradingNotPermitted, graderoster_for_section, section, user, user)
