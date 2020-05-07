e0043b38a55d7416917fa66fca5f9cc620f91d2d
Jim Laney <jlaney@uw.edu>
2020-05-07 11:24:57 -0700

    adds test settings for mci deployments

--- course_grader/cache.py
+++ course_grader/cache.py
@@ -1,54 +1,38 @@
 from rc_django.cache_implementation import TimedCache
-from rc_django.models import CacheEntryTimed
+from rc_django.cache_implementation.memcache import MemcachedCache
 import re
+
+ONE_MINUTE = 60
+ONE_HOUR = 60 * 60
+ONE_DAY = 60 * 60 * 24
+
+
+def get_cache_time(service, url):
+    if 'sws' == service:
+        if re.match(r'^/student/v\d/term/current', url):
+            return ONE_MINUTE
+        if re.match(r'^/student/v\d/term/', url):
+            return ONE_DAY
+        if re.match(r'^/student/v\d/course/', url):
+            return 10 * ONE_MINUTE
+        if re.match(r'^/student/v\d/section', url):
+            return 10 * ONE_MINUTE
+
+    if 'pws' == service:
+        return ONE_DAY
+
+    return ONE_MINUTE
 
 
 class RestClientsCache(TimedCache):
-    """ A custom cache implementation for GradePage """
-
-    url_policies = {}
-    url_policies["sws"] = (
-        (re.compile(r"^/student/v\d/term/current"), 60 * 60),
-        (re.compile(r"^/student/v\d/term/"), 60 * 60 * 10),
-        (re.compile(r"^/student/v\d/course/"), 60 * 10),
-        (re.compile(r"^/student/v\d/section"), 60 * 10),
-    )
-    url_policies["pws"] = (
-        (re.compile(r"^/identity/v\d/person/"), 60 * 60 * 10),
-        (re.compile(r"^/identity/v\d/entity/"), 60 * 60 * 10)
-    )
-    url_policies["gws"] = (
-        (re.compile(r"^/group_sws/v\d/group/"), 60 * 2),
-    )
-
     def getCache(self, service, url, headers):
-        cache_time = self.getCacheTime(service, url)
-        if cache_time is not None:
-            return self._response_from_cache(service, url, headers, cache_time)
-        else:
-            return None
-
-    def deleteCache(self, service, url):
-        try:
-            entry = CacheEntryTimed.objects.get(service=service, url=url)
-            entry.delete()
-        except CacheEntryTimed.DoesNotExist:
-            return
+        return self._response_from_cache(
+            service, url, headers, get_cache_time(service, url))
 
     def processResponse(self, service, url, response):
-        if self.getCacheTime(service, url) is not None:
-            return self._process_response(service, url, response)
-        else:
-            return
+        return self._process_response(service, url, response)
 
-    def getCacheTime(self, service, url):
-        if service in RestClientsCache.url_policies:
-            service_policies = RestClientsCache.url_policies[service]
 
-            for policy in service_policies:
-                pattern = policy[0]
-                policy_cache_time = policy[1]
-
-                if pattern.match(url):
-                    return policy_cache_time
-        return
+class RestClientsMemcachedCache(MemcachedCache):
+    def get_cache_expiration_time(self, service, url):
+        return get_cache_time(service, url)
