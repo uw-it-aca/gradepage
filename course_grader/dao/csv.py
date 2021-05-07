028d65c2aee92f084ceac4f8292410cb41c194fe
Jim Laney <jlaney@uw.edu>
2021-05-07 09:37:22 -0700

    license check

--- course_grader/dao/csv.py
+++ course_grader/dao/csv.py
@@ -1,3 +1,6 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from course_grader.dao.person import person_from_netid
 from restclients_core.exceptions import InvalidNetID, DataFailureException
 from logging import getLogger
