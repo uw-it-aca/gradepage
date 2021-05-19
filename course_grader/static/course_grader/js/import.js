7c1e032894b8c77ae7202f5ee945229685c7a6fb
Jim Laney <jlaney@uw.edu>
2021-05-19 09:19:00 -0700

    add section context to upload dialog

--- course_grader/static/course_grader/js/import.js
+++ course_grader/static/course_grader/js/import.js
@@ -262,6 +262,11 @@ GradePage.Import = (function ($) {
     function draw_upload_prompt(data) {
         var template = Handlebars.compile($("#upload-tmpl").html());
 
+        // Add context from the graderoster
+        data.section_name = window.gradepage.section_name;
+        data.expected_grade_count = $(".gp-roster-list").find(
+            GradePage.GradeRoster.grade_input_selector()).length;
+
         $(".gp-import-selector select").val("");
         $("#gp-import-modal-body").html(template(data));
         $("#gp-import-modal").modal({backdrop: "static"});
