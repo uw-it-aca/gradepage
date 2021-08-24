70b8b02832633b06ecc3a18b79dde4b04b4ee039
Jim Laney <jlaney@uw.edu>
2021-08-24 09:29:33 -0700

    add tests for xml parse error

--- course_grader/test/dao/test_graderoster.py
+++ course_grader/test/dao/test_graderoster.py
@@ -7,7 +7,8 @@ from uw_pws.util import fdao_pws_override
 from uw_sws.util import fdao_sws_override
 from course_grader.dao.person import PWS
 from course_grader.dao.section import get_section_by_label
-from course_grader.dao.graderoster import graderoster_for_section
+from course_grader.dao.graderoster import (
+    graderoster_for_section, DataFailureException)
 from course_grader.models import SubmittedGradeRoster
 from course_grader.exceptions import (
     GradingNotPermitted, ReceiptNotFound, GradingPeriodNotOpen)
@@ -20,15 +21,18 @@ class GraderosterDAOFunctionsTest(TestCase):
         section = get_section_by_label('2013,spring,TRAIN,101/A')
         user = PWS().get_person_by_regid('FBB38FE46A7C11D5A4AE0004AC494FFE')
         gr = graderoster_for_section(section, user, user)
-
         self.assertEqual(len(gr.items), 1000)
+
+        section = get_section_by_label('2013,spring,TRAIN,101/B')
+        gr = graderoster_for_section(section, user, user)
+        self.assertEqual(len(gr.items), 2)
 
     def test_submitted_graderoster_for_section(self):
         section = get_section_by_label('2013,spring,TRAIN,101/B')
         user = PWS().get_person_by_regid('FBB38FE46A7C11D5A4AE0004AC494FFE')
 
         gr = graderoster_for_section(section, user, user)
-        self.assertEqual(len(gr.items), 1)
+        self.assertEqual(len(gr.items), 2)
         self.assertFalse(hasattr(gr, 'submission_id'))
 
         model = SubmittedGradeRoster(
@@ -44,8 +48,14 @@ class GraderosterDAOFunctionsTest(TestCase):
         model.save()
 
         gr = graderoster_for_section(section, user, user)
-        self.assertEqual(len(gr.items), 1)
+        self.assertEqual(len(gr.items), 2)
         self.assertFalse(hasattr(gr, 'submission_id'))
+
+        # Test handing of etree.XMLSyntaxError
+        model.document = '<' + model.document
+        model.save()
+        self.assertRaises(DataFailureException, graderoster_for_section,
+                          section, user, user)
 
     def test_submitted_only_graderoster_for_section(self):
         section = get_section_by_label('2013,spring,TRAIN,101/B')
@@ -71,7 +81,7 @@ class GraderosterDAOFunctionsTest(TestCase):
 
         gr = graderoster_for_section(
             section, user, user, submitted_graderosters_only=True)
-        self.assertEqual(len(gr.items), 1)
+        self.assertEqual(len(gr.items), 2)
         self.assertEqual(gr.submission_id, 'B')
 
     def test_graderoster_not_permitted(self):
