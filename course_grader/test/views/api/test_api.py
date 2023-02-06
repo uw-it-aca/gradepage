a72f153dff0972263493aa71f412b01798fb10b9
Jim Laney <jlaney@uw.edu>
2023-02-06 14:06:09 -0800

    update copyright

--- course_grader/test/views/api/test_api.py
+++ course_grader/test/views/api/test_api.py
@@ -1,5 +1,6 @@
-# Copyright 2022 UW-IT, University of Washington
+# Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
+
 
 from django.test import TestCase, override_settings
 from course_grader.dao.person import PWS
--- course_grader/views/__init__.py
+++ course_grader/views/__init__.py
@@ -1,5 +1,6 @@
-# Copyright 2022 UW-IT, University of Washington
+# Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
+
 
 from django.urls import reverse
 from course_grader.dao import display_datetime
