a4633227003977927f07257abf35b43d8a2afec3
Jim Laney <jlaney@uw.edu>
2024-07-15 16:42:47 -0700

    add vue secrets

--- course_grader_vue/utils/data.js
+++ course_grader_vue/utils/data.js
@@ -31,6 +31,55 @@ function _handleError(error) {
   }
 }
 
-export {
+async function getSections(url) {
+  return axios.get(url).catch(_handleError);
+}
 
+async function getSectionStatus(url) {
+  return axios.get(url);
+}
+
+async function getGraderoster(url) {
+  return axios.get(url).catch(_handleError);
+}
+
+async function updateGraderoster(url, data) {
+  return axios.put(url, data).catch(_handleError);
+}
+
+async function submitGraderoster(url, data) {
+  return axios.post(url, data).catch(_handleError);
+}
+
+async function updateGrade(url, data) {
+  return axios.patch(url, data);
+}
+
+async function createImport(url, data) {
+  return axios.post(url, data).catch(_handleError);
+}
+
+async function saveImportedGrades(url, data) {
+  return axios.put(url, data).catch(_handleError);
+}
+
+async function uploadGrades(url, data) {
+  return axios.post(url, data).catch(_handleError);
+}
+
+async function getConversionScales(url) {
+  return axios.get(url).catch(_handleError);
+}
+
+export {
+  getSections,
+  getSectionStatus,
+  getGraderoster,
+  updateGraderoster,
+  submitGraderoster,
+  updateGrade,
+  createImport,
+  saveImportedGrades,
+  uploadGrades,
+  getConversionScales,
 };
