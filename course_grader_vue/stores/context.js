7f372ca6454223974642442ca737f6297ed9a1a4
Jim Laney <jlaney@uw.edu>
2024-07-17 16:17:27 -0700

    add override api

--- course_grader_vue/stores/context.js
+++ course_grader_vue/stores/context.js
@@ -3,13 +3,14 @@ import { defineStore } from "pinia";
 export const useContextStore = defineStore({
   id: "django-context",
   state: () => {
-    console.log(document.getElementById("django-context-data").textContent);
     return {
       name: "Context",
       context: JSON.parse(
         document.getElementById("django-context-data").textContent
       ),
-      messages: {},
+      messages: JSON.parse(
+        document.getElementById("persistent-message-data").textContent
+      ),
     };
   },
   getters: {},
