930adbb0773f5261dd7c6534f482f0690393f7c2
Jim Laney <jlaney@uw.edu>
2024-07-26 10:21:11 -0700

    add section status and term selector

--- course_grader_vue/pages/term.vue
+++ course_grader_vue/pages/term.vue
@@ -1,16 +1,31 @@
 <template>
-  <layout :page-title="pageTitle">
+  <layout :page-title="selectedTermText">
     <template #content>
+      <div>
+        <select
+          aria-label="Select term"
+          @change="selectTerm"
+          v-model="selectedTermUrl"
+        >
+          <template v-for="term in this.contextStore.context.terms" :key="term.id">
+            <option
+              :value="term.url"
+              :title="`Select ${term.quarter} ${term.year}`"
+              :selected="term.is_selected"
+            >{{ term.quarter }} {{ term.year }}</option>
+          </template>
+        </select>
+      </div>
       <div class="border">
-      <div v-if="sections.length > 0">
-        <section-list :sections="sections"></section-list>
+        <div v-if="sections.length > 0">
+          <section-list :sections="sections"></section-list>
+        </div>
+        <div v-else>
+          You do not have any classes to grade for <strong>{{ selectedTermText }}</strong
+          >. If you believe this to be incorrect, please contact your department's
+          Time Schedule Coordinator.
+        </div>
       </div>
-      <div v-else>
-        You do not have any classes to grade for <strong>{{ pageTitle }}</strong
-        >. If you believe this to be incorrect, please contact your department's
-        Time Schedule Coordinator.
-      </div>
-    </div>
     </template>
   </layout>
 </template>
@@ -35,26 +50,31 @@ export default {
   },
   data() {
     return {
-      pageTitle: this.contextStore.context.page_title,
+      selectedTermUrl: "",
+      selectedTermText: this.contextStore.context.page_title,
       sectionsURL: this.contextStore.context.sections_url,
       sections: [],
     };
   },
   methods: {
+    selectTerm: function (e) {
+      this.contextStore.selectTerm(e.target.value);
+      window.location.href = this.contextStore.context.terms.find((t) =>
+        t.is_selected).url;
+    },
     updateTerm: function () {
       let term;
       if (this.$route.params.id) {
         term = this.contextStore.context.terms.find((t) =>
           t.sections_url.endsWith(this.$route.params.id)
         );
-        if (term) {
-          this.pageTitle = term.quarter + " " + term.year;
-          this.sectionsURL = term.sections_url;
-        }
-      } else {
-        this.pageTitle = this.contextStore.context.page_title;
-        this.sectionsURL = this.contextStore.context.sections_url;
       }
+      if (!term) {
+        term = this.contextStore.context.terms[0];
+      }
+      this.selectedTermUrl = term.url;
+      this.selectedTermText = term.quarter + " " + term.year;
+      this.sectionsURL = term.sections_url;
     },
     loadSectionsForTerm: function () {
       this.getSections(this.sectionsURL)
