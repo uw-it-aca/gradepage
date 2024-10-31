9f06fa29dcfc9dc4517d022e1b589aa5c049dcfc
Jim Laney <jlaney@uw.edu>
2024-10-31 08:42:08 -0700

    clean up layout data

--- course_grader_vue/pages/term.vue
+++ course_grader_vue/pages/term.vue
@@ -112,7 +112,7 @@ export default {
         );
       }
       if (!term) {
-        term = this.contextStore.context.terms[0];
+        term = this.currentTerm;
       }
       this.selectedTerm = term;
     },
