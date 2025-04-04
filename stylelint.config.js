ac1c31847d9a202ab06df018f1527360ac5b6ecb
Charlon Palacay <charlon@gmail.com>
2025-04-04 14:47:37 -0700

    Updates stylelint config.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,23 @@
-{
-  "extends": [
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
     "stylelint-config-standard-scss",
-    "stylelint-config-recommended-vue/scss"
+    "stylelint-config-recommended-vue/scss",
   ],
-  "rules": {
-    "scss/no-global-function-names": null,
-    "no-invalid-position-at-import-rule": null
-  }
-}
+  customSyntax: "postcss-html",
+  overrides: [
+    {
+      files: ["*.vue", "**/*.vue"],
+      rules: {
+        "scss/no-global-function-names": null,
+        "no-invalid-position-at-import-rule": null,
+        "scss/at-import-partial-extension": [
+          "never",
+          {
+            severity: "warning",
+          },
+        ],
+      },
+    },
+  ],
+};
