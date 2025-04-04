2c1b908dbe525f202e3022720ebd39070377da4d
Charlon Palacay <charlon@gmail.com>
2025-04-04 14:48:20 -0700

    Fix prettier config.

--- prettier.config.js
+++ prettier.config.js
@@ -1,23 +1,12 @@
-/** @type {import('stylelint').Config} */
-export default {
-  extends: [
-    "stylelint-config-standard-scss",
-    "stylelint-config-recommended-vue/scss",
-  ],
-  customSyntax: "postcss-html",
-  overrides: [
-    {
-      files: ["*.vue", "**/*.vue"],
-      rules: {
-        "scss/no-global-function-names": null,
-        "no-invalid-position-at-import-rule": null,
-        "scss/at-import-partial-extension": [
-          "never",
-          {
-            severity: "warning",
-          },
-        ],
-      },
-    },
-  ],
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
 };
+
+export default config;
