be23f7b42a45c1b7d5ec56b5ba824e662ec2bd62
Jim Laney <jlaney@uw.edu>
2025-06-29 13:44:42 -0700

    add component for section inline grading status

--- course_grader_vue/components/workflow/edit-grades.vue
+++ course_grader_vue/components/workflow/edit-grades.vue
@@ -24,73 +24,14 @@
     </template>
   </div>
 
+  <!-- submission inline status -->
+  <SectionGradingStatus
+    v-if="appState.graderoster"
+    :graderoster="appState.graderoster"
+    :saved-grades="hasSubmittedAndSavedGrades"
+  />
+
   <template v-if="appState.graderoster">
-    <!-- submission inline status -->
-    <template v-if="appState.graderoster.has_successful_submissions">
-      <BCard bg-variant="body-tertiary" class="border-0 mb-4">
-        <div v-if="hasSubmittedAndSavedGrades" class="border-bottom mb-2 pb-2">
-          <span class="fw-bold"
-            ><i class="bi bi-exclamation-triangle-fill text-warning me-2"></i
-            >Resubmit to make any changes official.</span
-          >
-          Otherwise, the most recent grade submission will stand.
-        </div>
-        <!-- check if secondary section -->
-        <div class="d-flex justify-content-between">
-          <div>
-            <div class="fw-bold ms-4">Course sections submitted: x of x</div>
-            <div class="fst-italic small ms-4">
-              Submitted grades may differ from official final grade.
-              <BLink
-                to="https://registrar.washington.edu/staff-faculty/grading-resources/"
-                >Learn why</BLink
-              >.
-            </div>
-          </div>
-          <div>
-            <BButton v-b-toggle.collapse-1 variant="quiet-primary" size="sm"
-              >Show sections</BButton
-            >
-          </div>
-        </div>
-        <BCollapse id="collapse-1">
-          <ul class="list-unstyled mt-2 ms-4">
-            <li
-              v-for="(submission, index) in appState.graderoster.submissions"
-              :key="index"
-            >
-              <i class="bi bi-check-circle-fill text-success me-2"></i>
-              <span v-if="submission.section_id">
-                Section {{ submission.section_id }}:
-              </span>
-              <span v-html="gradesSubmittedText(submission)"></span>
-            </li>
-          </ul>
-        </BCollapse>
-
-        <!-- else primary -->
-        <ul class="list-unstyled m-0">
-          <li
-            v-for="(submission, index) in appState.graderoster.submissions"
-            :key="index"
-          >
-            <i class="bi bi-check-circle-fill text-success me-2"></i>
-            <span v-if="submission.section_id">
-              Section {{ submission.section_id }}:
-            </span>
-            <span v-html="gradesSubmittedText(submission)"></span>
-          </li>
-        </ul>
-        <div class="fst-italic small ms-4">
-          Submitted grades may differ from official final grade.
-          <BLink
-            to="https://registrar.washington.edu/staff-faculty/grading-resources/"
-            >Learn why</BLink
-          >.
-        </div>
-      </BCard>
-    </template>
-
     <table v-if="appState.graderoster.students" class="table table-striped">
       <thead class="table-body-secondary">
         <tr>
@@ -109,7 +50,7 @@
         </tr>
       </tbody>
     </table>
-    <div v-else-if="!errorResponse">Loading something....</div>
+    <div v-else-if="!errorResponse">Loading roster...</div>
 
     <div class="d-flex justify-content-between mb-4">
       <div class="w-75">
@@ -151,6 +92,7 @@
 
 <script>
 import SectionHeader from "@/components/section/header.vue";
+import SectionGradingStatus from "@/components/section/grading-status.vue";
 import Student from "@/components/student.vue";
 import Errors from "@/components/errors.vue";
 import GradeImportOptions from "@/components/import/source-options.vue";
@@ -158,22 +100,19 @@ import { useWorkflowStateStore } from "@/stores/state";
 import { useGradeStore } from "@/stores/grade";
 import { updateGraderoster, clearSavedGrades } from "@/utils/data";
 import {
-  gradesSubmittedText,
-  //duplicateCodeText,
   writingSectionText,
 } from "@/utils/grade";
-import { BAlert, BButton, BCard, BPlaceholder } from "bootstrap-vue-next";
+import { BAlert, BButton } from "bootstrap-vue-next";
 
 export default {
   components: {
     SectionHeader,
+    SectionGradingStatus,
     GradeImportOptions,
     Student,
     Errors,
     BAlert,
     BButton,
-    BCard,
-    BPlaceholder,
   },
   props: {
     section: {
@@ -189,8 +128,6 @@ export default {
       gradeStore,
       updateGraderoster,
       clearSavedGrades,
-      gradesSubmittedText,
-      //duplicateCodeText,
       writingSectionText,
     };
   },
