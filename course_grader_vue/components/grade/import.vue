41ea0709b406b5c2f249496d7fa89a29aa9d420a
Jim Laney <jlaney@uw.edu>
2025-07-23 20:18:35 -0700

    update conversion table

--- course_grader_vue/components/grade/import.vue
+++ course_grader_vue/components/grade/import.vue
@@ -1,21 +1,9 @@
 <template>
   <div>
-    <span v-if="student.is_override_grade">
-      <span
-        class="fa-stack"
-        title="Override grade imported from Canvas Gradebook"
-      >
-        <i class="fas fa-circle fa-stack-2x" aria-hidden="true"></i>
-        <i
-          class="fas fa-marker fa-flip-horizontal fa-stack-1x fa-inverse"
-          aria-hidden="true"
-        ></i>
-      </span>
-    </span>
-    {{ student.imported_grade }}%
-    <span class="visually-hidden">converted to</span>
-    <i class="fas fa-long-arrow-alt-right"></i>
-    <strong>{{ student.converted_grade }}</strong>
+    <span class="fs-2 fw-bold">{{ student.converted_grade }}</span>
+  </div>
+  <div v-if="student.is_override_grade" class="small">
+    Override grade imported from Canvas Gradebook
   </div>
 </template>
 
