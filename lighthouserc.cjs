8a5a844f50abcd81f5817a093e927ff11a51117a
Charlon Palacay <charlon@gmail.com>
2025-08-08 16:16:56 -0700

    Updates lighthouse config.

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -8,11 +8,17 @@ module.exports = {
       settings: {
         //set which categories you want to run
         onlyCategories: ["accessibility"],
+        preset: "desktop",
+        extraHeaders: '{"Sec-CH-Prefers-Color-Scheme": "light"}',
       },
       url: [
         // add URLs to be tested (usually matches /pages structure)
-        `http://localhost:${port}/`,
-        `http://localhost:${port}/customize`,
+        `http://localhost:${port}/term/2013-winter`,
+        `http://localhost:${port}/term/2013-spring`,
+        `http://localhost:${port}/section/2013-spring-A%20B&C-101-A-FBB38FE46A7C11D5A4AE0004AC494FFE`,
+        `http://localhost:${port}/section/2013-spring-TRAIN-101-B-FBB38FE46A7C11D5A4AE0004AC494FFE`,
+        `http://localhost:${port}/section/2013-spring-ZERROR-101-C-FBB38FE46A7C11D5A4AE0004AC494FFE`,
+        `http://localhost:${port}/section/013-spring-ZERROR-101-S1-FBB38FE46A7C11D5A4AE0004AC494FFE`,
       ],
       // specify other options like numberOfRuns, staticDistDir, etc.
       numberOfRuns: 1,
