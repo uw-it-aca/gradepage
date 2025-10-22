927fdbab4ea3317070684794de9bb9369d15895f
Jim Laney <jlaney@uw.edu>
2025-10-22 15:04:13 -0700

    handle missing items from returned graderoster

--- course_grader/dao/gradesubmission.py
+++ course_grader/dao/gradesubmission.py
@@ -44,43 +44,54 @@ def submit_grades(model):
     # item. To create a saved graderoster receipt, merge the returned
     # graderoster into the submitted graderoster, to capture both the
     # submitted grade and the returned status code/message for each item.
+    ret_graderoster__dict = {}
     for item in ret_graderoster.items:
-        if item.status_code is None:
-            continue
+        key = item.student_label(separator="-")
+        ret_graderoster_dict[key] = item
+
+    for item in graderoster.items:
+        key = item.student_label(separator="-")
+
+        # Capture the submitted grade for logging
+        submitted_grade = format_logged_grade(item)
 
         try:
-            idx = graderoster.items.index(item)
+            ret_item = ret_graderoster_dict[key]
+        except KeyError:
+            logger.info((
+                "Grade submitted, Student: {student}, Section: "
+                "{section_id}, Submitted grade: {submitted_grade}, "
+                "Returned grade: {returned_grade}, Code: {status_code}, "
+                "Message: {message}").format(
+                    student=key,
+                    section_id=logged_section_id,
+                    submitted_grade=submitted_grade,
+                    returned_grade=None,
+                    status_code=None,
+                    message=None))
+            continue
 
-            # Capture the submitted grade for logging
-            submitted_grade = format_logged_grade(graderoster.items[idx])
+        # Update the graderoster with data returned from PUT
+        # item.grade = ret_item.grade
+        # item.no_grade_now = ret_item.no_grade_now
+        # item.has_incomplete = ret_item.has_incomplete
+        # item.has_writing_credit = ret_item.has_writing_credit
+        item.status_code = ret_item.status_code
+        item.status_message = ret_item.status_message
+        item.date_graded = ret_item.date_graded
+        item.grade_document_id = ret_item.grade_document_id
+        item.grade_submitter_source = ret_item.grade_submitter_source
 
-            graderoster.items[idx].grade = item.grade
-            graderoster.items[idx].no_grade_now = item.no_grade_now
-            graderoster.items[idx].has_incomplete = item.has_incomplete
-            graderoster.items[idx].has_writing_credit = item.has_writing_credit
-            graderoster.items[idx].status_code = item.status_code
-            graderoster.items[idx].status_message = item.status_message
-            graderoster.items[idx].date_graded = item.date_graded
-            graderoster.items[idx].grade_document_id = item.grade_document_id
-            graderoster.items[idx].grade_submitter_source = (
-                item.grade_submitter_source)
-
-            if submitted_grade is not None:
-                logger.info((
-                    "Grade submitted, Student: {student}, Section: "
-                    "{section_id}, Submitted grade: {submitted_grade}, "
-                    "Returned grade: {returned_grade}, Code: {status_code}, "
-                    "Message: {message}").format(
-                        student=graderoster.items[idx].student_label(
-                            separator="-"),
-                        section_id=logged_section_id,
-                        submitted_grade=submitted_grade,
-                        returned_grade=format_logged_grade(
-                            graderoster.items[idx]),
-                        status_code=graderoster.items[idx].status_code,
-                        message=graderoster.items[idx].status_message))
-
-        except Exception as ex:
-            logger.error("Error logging grade: {}".format(ex))
+        logger.info((
+            "Grade submitted, Student: {student}, Section: "
+            "{section_id}, Submitted grade: {submitted_grade}, "
+            "Returned grade: {returned_grade}, Code: {status_code}, "
+            "Message: {message}").format(
+                student=key,
+                section_id=logged_section_id,
+                submitted_grade=submitted_grade,
+                returned_grade=format_logged_grade(ret_item),
+                status_code=item.status_code,
+                message=item.status_message))
 
     return graderoster
